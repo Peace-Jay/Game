93a2717f2a9f0a58e9d107109134cd4747bed876
peace jay <alebiosupeace@gmail.com>
2023-12-17 21:56:25 +0100

    GAME

--- GAME.py
+++ GAME.py
@@ -15,10 +15,14 @@ for i in Game:
     hint = i["hint"]
     print("hint:", hint)
 
-    word_guess = input("guess the word:")
-
-    if word_guess == i["word"]:
-        print("Correct!\n")
-    else:
-        print("Incorrect. The correct word is:",  i["word"], "\n")
-        print("hint:", hint, "\n")
+    for guess in range(3):
+        word_guess = input("guess the word:".format(guess + 1))
+        if word_guess.lower() == i["word"].lower():
+            print("Correct!\n")
+            break
+        else:
+            if guess < 2:
+                print("Incorrect. Try again")
+            else:
+                print("Incorrect. You have reached your max number of attempt")
+                print("The correct word is:", i["word"], "\n")
